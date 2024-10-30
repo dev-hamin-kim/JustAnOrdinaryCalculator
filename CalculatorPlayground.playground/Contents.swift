6994c02b7d9deb29db99a36eb6f7ee7d3a11c52c
Hamin Kim <dev.hamin.kim@gmail.com>
2024-10-30 12:50:17 +0900

    [New] Basic operations made in CalculatorPlayground (Lv. 1)

--- CalculatorPlayground.playground/Contents.swift
+++ CalculatorPlayground.playground/Contents.swift
@@ -1,3 +1,28 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+class Calculator {
+    
+    func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+}
+
+let calculator = Calculator()
+
+calculator.add(lhs: 1, rhs: 3)
+calculator.divide(lhs: 4, rhs: 2)
+calculator.multiply(lhs: 1, rhs: 4)
+calculator.subtract(lhs: 14, rhs: 4)
